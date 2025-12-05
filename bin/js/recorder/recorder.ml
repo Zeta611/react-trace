ffb0a6d08cb734725e2749f33df89006ccbaf0d1
Jay Lee <jaeho.lee@snu.ac.kr>
2025-12-06 02:41:45 +0900

    :sparkles: Show view info on nodes

--- bin/js/recorder/recorder.ml
+++ bin/js/recorder/recorder.ml
@@ -17,10 +17,25 @@ let get_path_from_checkpoint = function
       Some pt
   | Event _ -> None
 
-type tree = { path : string; name : string; children : tree list }
-and view = { msg : string; tree : tree }
+type state_entry = { label : string; value : string; queue_size : int }
+[@@deriving yojson_of]
 
-and recording = {
+type decision_info = { chk : bool; eff : bool } [@@deriving yojson_of]
+
+type tree = {
+  path : string;
+  name : string;
+  children : tree list;
+  st_store : state_entry list option;
+  eff_q_size : int option;
+  dec : decision_info option;
+  arg : string option;
+}
+[@@deriving yojson_of]
+
+type view = { msg : string; tree : tree } [@@deriving yojson_of]
+
+type recording = {
   checkpoints : view list;
   root : (Concrete_domains.tree[@yojson.opaque]) option;
   log : string;
@@ -30,13 +45,58 @@ and recording = {
 let emp_recording = { checkpoints = []; root = None; log = "" }
 
 let leaf : const -> tree = function
-  | Unit -> { path = ""; name = "()"; children = [] }
-  | Bool b -> { path = ""; name = Bool.to_string b; children = [] }
-  | Int i -> { path = ""; name = Int.to_string i; children = [] }
-  | String s -> { path = ""; name = s; children = [] }
+  | Unit ->
+      {
+        path = "";
+        name = "()";
+        children = [];
+        st_store = None;
+        eff_q_size = None;
+        dec = None;
+        arg = None;
+      }
+  | Bool b ->
+      {
+        path = "";
+        name = Bool.to_string b;
+        children = [];
+        st_store = None;
+        eff_q_size = None;
+        dec = None;
+        arg = None;
+      }
+  | Int i ->
+      {
+        path = "";
+        name = Int.to_string i;
+        children = [];
+        st_store = None;
+        eff_q_size = None;
+        dec = None;
+        arg = None;
+      }
+  | String s ->
+      {
+        path = "";
+        name = s;
+        children = [];
+        st_store = None;
+        eff_q_size = None;
+        dec = None;
+        arg = None;
+      }
 
 let clos : clos -> tree = function
-  | _ -> { path = ""; name = "button"; children = [] }
+  | _ ->
+      {
+        path = "";
+        name = "button";
+        children = [];
+        st_store = None;
+        eff_q_size = None;
+        dec = None;
+        arg = None;
+      }
 
 let rec tree : Concrete_domains.tree -> tree = function
   | T_const k -> leaf k
@@ -45,14 +105,42 @@ let rec tree : Concrete_domains.tree -> tree = function
   | T_path p -> path p
 
 and list (ts : Concrete_domains.tree list) : tree =
-  { path = ""; name = "..."; children = List.map ts ~f:tree }
+  {
+    path = "";
+    name = "...";
+    children = List.map ts ~f:tree;
+    st_store = None;
+    eff_q_size = None;
+    dec = None;
+    arg = None;
+  }
 
 and path (pt : Path.t) : tree =
-  let { comp_spec = { comp; _ }; children; _ } = perform (Lookup_view pt) in
+  let { comp_spec = { comp; arg }; dec; st_store; eff_q; children } =
+    perform (Lookup_view pt)
+  in
+  (* Convert st_store to state_entry list *)
+  let st_store_entries =
+    St_store.to_alist st_store
+    |> List.map ~f:(fun (lbl, (v, q)) ->
+           {
+             label = lbl;
+             value = Sexp.to_string (sexp_of_value v);
+             queue_size = Job_q.size q;
+           })
+  in
+  (* Convert decision *)
+  let dec_info = { chk = dec.chk; eff = dec.eff } in
+  (* Convert arg to string *)
+  let arg_str = Sexp.to_string (sexp_of_value arg) in
   {
     path = pt |> Path.sexp_of_t |> Sexp.to_string;
     name = comp;
     children = [ tree children ];
+    st_store = Some st_store_entries;
+    eff_q_size = Some (Job_q.size eff_q);
+    dec = Some dec_info;
+    arg = Some arg_str;
   }
 
 let event_h (type a b) (f : a -> b) (x : a) :
--- bin/js/recorder/recorder.mli
+++ bin/js/recorder/recorder.mli
@@ -1,7 +1,22 @@
-type tree = { path : string; name : string; children : tree list }
-and view = { msg : string; tree : tree } [@@deriving yojson_of]
+type state_entry = { label : string; value : string; queue_size : int }
+[@@deriving yojson_of]
 
-and recording = {
+type decision_info = { chk : bool; eff : bool } [@@deriving yojson_of]
+
+type tree = {
+  path : string;
+  name : string;
+  children : tree list;
+  st_store : state_entry list option;
+  eff_q_size : int option;
+  dec : decision_info option;
+  arg : string option;
+}
+[@@deriving yojson_of]
+
+type view = { msg : string; tree : tree } [@@deriving yojson_of]
+
+type recording = {
   checkpoints : view list;
   root : Lib_domains.Concrete_domains.tree option;
   log : string;
